9b966c5127ba36427047ec0750776ca83ec51155
Ahmed El-Hassany <eahmed@ethz.ch>
2015-09-10 15:16:39 +0200

    better organization of consistency app and add timer for updating version

--- pox/forwarding/consistency.py
+++ pox/forwarding/consistency.py
@@ -5,6 +5,7 @@ from pox.lib.util import dpidToStr
 from pox.lib.util import str_to_bool
 from pox.lib.addresses import EthAddr
 from pox.lib.addresses import IPAddr
+from pox.lib.recoco.recoco import Timer
 import time
 
 log = core.getLogger()
@@ -82,9 +83,7 @@ class InternetSwitch(EventMixin):
       msg.actions.append(of.ofp_action_output(port=host_ports[host]))
       self.connection.send(msg)
 
-  def install_v1(self):
-    self.install_internal()
-    # Just send everything else to F2
+  def all_to_f2(self):
     out_priority = 100
     internet_msg = of.ofp_flow_mod()
     internet_msg.match = of.ofp_match()
@@ -92,6 +91,11 @@ class InternetSwitch(EventMixin):
     internet_msg.priority = out_priority
     self.log.info("")
     self.connection.send(internet_msg)
+
+  def install_v1(self):
+    self.install_internal()
+    # Just send everything else to F2
+    self.all_to_f2()
 
   def install_v2(self):
     """No change"""
@@ -101,7 +105,6 @@ class InternetSwitch(EventMixin):
     """
     Handles packet in messages from the switch to implement above algorithm.
     """
-
     packet = event.parse()
     self.log.info("Ignoring packet in at Internet Switch: %s", str(packet))
 
@@ -126,71 +129,28 @@ class InternalSwitch(EventMixin):
       self.log.debug("Installing v1 rule: %s", str(msg).replace("\n", " "))
       self.connection.send(msg)
 
+  def redirect_traffic(self, src, port, priority=100):
+    msg = of.ofp_flow_mod()
+    msg.match = of.ofp_match(nw_src=host_ips[src])
+    msg.actions.append(of.ofp_action_output(port=port))
+    msg.priority = priority
+    self.connection.send(msg)
+
   def install_v1(self):
     self.log.info("Installing v1")
     self.install_internal()
-    out_priority = 100
-    # Faculty Rules
-    faculty_msg = of.ofp_flow_mod()
-    faculty_msg.match = of.ofp_match(nw_src=host_ips[faculty])
-    faculty_msg.actions.append(of.ofp_action_output(port=internal_ports[f3]))
-    faculty_msg.priority = out_priority
-
-    # Student Rules
-    student_msg = of.ofp_flow_mod()
-    student_msg.match = of.ofp_match(nw_src=host_ips[student])
-    student_msg.actions.append(of.ofp_action_output(port=internal_ports[f2]))
-    student_msg.priority = out_priority
-
-    # Guest Rules
-    guest_msg = of.ofp_flow_mod()
-    guest_msg.match = of.ofp_match(nw_src=host_ips[guest])
-    guest_msg.actions.append(of.ofp_action_output(port=internal_ports[f1]))
-    guest_msg.priority = out_priority
-
-    # Unkown Rules
-    unkown_msg = of.ofp_flow_mod()
-    unkown_msg.match = of.ofp_match(nw_src=host_ips[unknown])
-    unkown_msg.actions.append(of.ofp_action_output(port=internal_ports[f1]))
-    unkown_msg.priority = out_priority
-
-    self.connection.send(faculty_msg)
-    self.connection.send(student_msg)
-    self.connection.send(guest_msg)
-    self.connection.send(unkown_msg)
+    self.redirect_traffic(faculty, internal_ports[f3])
+    self.redirect_traffic(student, internal_ports[f2])
+    self.redirect_traffic(guest, internal_ports[f1])
+    self.redirect_traffic(unknown, internal_ports[f1])
 
   def install_v2(self):
     self.log.info("Installing v2")
     self.install_internal()
-    out_priority = 100
-    # Faculty Rules
-    faculty_msg = of.ofp_flow_mod()
-    faculty_msg.match = of.ofp_match(nw_src=host_ips[faculty])
-    faculty_msg.actions.append(of.ofp_action_output(port=internal_ports[f3]))
-    faculty_msg.priority = out_priority
-
-    # Student Rules
-    student_msg = of.ofp_flow_mod()
-    student_msg.match = of.ofp_match(nw_src=host_ips[student])
-    student_msg.actions.append(of.ofp_action_output(port=internal_ports[f3]))
-    student_msg.priority = out_priority
-
-    # Guest Rules
-    guest_msg = of.ofp_flow_mod()
-    guest_msg.match = of.ofp_match(nw_src=host_ips[guest])
-    guest_msg.actions.append(of.ofp_action_output(port=internal_ports[f2]))
-    guest_msg.priority = out_priority
-
-    # Unkown Rules
-    unkown_msg = of.ofp_flow_mod()
-    unkown_msg.match = of.ofp_match(nw_src=host_ips[unknown])
-    unkown_msg.actions.append(of.ofp_action_output(port=internal_ports[f1]))
-    unkown_msg.priority = out_priority
-
-    self.connection.send(faculty_msg)
-    self.connection.send(student_msg)
-    self.connection.send(guest_msg)
-    self.connection.send(unkown_msg)
+    self.redirect_traffic(faculty, internal_ports[f3])
+    self.redirect_traffic(student, internal_ports[f3])
+    self.redirect_traffic(guest, internal_ports[f2])
+    self.redirect_traffic(unknown, internal_ports[f1])
 
   def _handle_PacketIn (self, event):
     """
@@ -201,41 +161,54 @@ class InternalSwitch(EventMixin):
     self.log.info("Ignoring packet in at Internal Switch: %s", str(packet))
 
 
-class F1Switch(EventMixin):
+class FSwitch(EventMixin):
   def __init__ (self, connection):
     self.connection = connection
 
     # We want to hear PacketIn messages, so we listen
     self.listenTo(connection)
-    self.log = core.getLogger(self.__class__.__name__)
-    self.log.debug("Initializing F1 Switch")
 
-  def install_v1(self):
-    # Service1 Rules
-    s1_msg = of.ofp_flow_mod()
-    s1_msg.match = of.ofp_match(nw_src=host_ips[service1])
-    s1_msg.actions.append(of.ofp_action_output(port=fs_ports[monitor]))
-    s1_msg.priority = 1000
+  def redirect_serivce(self, ip, port, priority=1000):
+    msg = of.ofp_flow_mod()
+    msg.match = of.ofp_match(nw_src=ip)
+    msg.actions.append(of.ofp_action_output(port=port))
+    msg.priority = priority
+    self.connection.send(msg)
 
+  def allow_all(self, priority=100):
     # To internet Rules
     internet_msg = of.ofp_flow_mod()
     internet_msg.match = of.ofp_match(in_port=fs_ports[internal])
     internet_msg.actions.append(of.ofp_action_output(port=fs_ports[internet]))
-    internet_msg.priority = 100
+    internet_msg.priority = priority
 
     # To internal Rules
     internal_msg = of.ofp_flow_mod()
     internal_msg.match = of.ofp_match(in_port=fs_ports[internet])
     internal_msg.actions.append(of.ofp_action_output(port=fs_ports[internal]))
-    internal_msg.priority = 100
+    internal_msg.priority = priority
 
-    self.connection.send(s1_msg)
     self.connection.send(internal_msg)
     self.connection.send(internet_msg)
 
+
+class F1Switch(FSwitch):
+  def __init__ (self, connection):
+    super(F1Switch, self).__init__(connection)
+
+    self.log = core.getLogger(self.__class__.__name__)
+    self.log.debug("Initializing F1 Switch")
+
+  def install_v1(self):
+    self.log.info("Installing v1")
+    # Service1 Rules
+    self.redirect_serivce(host_ips[service1], fs_ports[monitor])
+    self.allow_all()
+
   def install_v2(self):
     self.log.info("Installing v2")
-    self.install_v1()
+    self.redirect_serivce(host_ips[service1], fs_ports[monitor])
+    self.allow_all()
 
   def _handle_PacketIn (self, event):
     """
@@ -246,12 +219,9 @@ class F1Switch(EventMixin):
     log.info("XXX Ignoring packet in at F1 Switch: %s", str(packet))
 
 
-class F2Switch(EventMixin):
+class F2Switch(FSwitch):
   def __init__ (self, connection):
-    self.connection = connection
-
-    # We want to hear PacketIn messages, so we listen
-    self.listenTo(connection)
+    super(F2Switch, self).__init__(connection)
     self.log = core.getLogger(self.__class__.__name__)
     self.log.debug("Initializing F2 Switch")
 
@@ -304,12 +274,9 @@ class F2Switch(EventMixin):
     log.info("XXX Ignoring packet in at F2 Switch: %s", str(packet))
 
 
-class F3Switch(EventMixin):
+class F3Switch(FSwitch):
   def __init__ (self, connection):
-    self.connection = connection
-
-    # We want to hear PacketIn messages, so we listen
-    self.listenTo(connection)
+    super(F3Switch, self).__init__(connection)
     self.log = core.getLogger("F3Switch")
     self.log.info("Initializing F3 Switch")
 
@@ -381,7 +348,6 @@ class MonitorSwitch(EventMixin):
     """
     Handles packet in messages from the switch to implement above algorithm.
     """
-
     packet = event.parse()
     self.log.info("XXX Ignoring packet in at MonitorSwitch Switch: %s and port %d", str(packet) , event.port)
 
@@ -392,33 +358,45 @@ class Main(EventMixin):
   """
   def __init__ (self):
     self.listenTo(core.openflow)
+    self.log = core.getLogger("Main")
     self.handlers = {}
+    self.timer = Timer(5, self.update_version)
+
+  def inconsistent_update(self):
+    self.log.info("XXX Inconsistent Update")
+    for name, sw in self.handlers.iteritems():
+      sw.install_v2()
+
+  def update_version(self):
+    self.log.info("XXX Update version triggered")
+    self.inconsistent_update()
+
   def _handle_ConnectionUp (self, event):
     log.debug("Connection %s", event.connection)
     dpid = dpidToStr(event.dpid)
     if dpid == internal:
       sw = InternalSwitch(event.connection)
-      sw.install_v2()
+      sw.install_v1()
       self.handlers[internal] = sw
     elif dpid == f1:
       sw = F1Switch(event.connection)
-      sw.install_v2()
+      sw.install_v1()
       self.handlers[f1] = sw
     elif dpid == f2:
       sw = F2Switch(event.connection)
-      sw.install_v2()
+      sw.install_v1()
       self.handlers[f2] = sw
     elif dpid == f3:
       sw = F3Switch(event.connection)
-      sw.install_v2()
+      sw.install_v1()
       self.handlers[f3] = sw
     elif dpid == internet:
       sw = InternetSwitch(event.connection)
-      sw.install_v2()
+      sw.install_v1()
       self.handlers[internet] = sw
     elif dpid == monitor:
       sw = MonitorSwitch(event.connection)
-      sw.install_v2()
+      sw.install_v1()
       self.handlers[monitor] = sw
     else:
       log.error("Unkown switch with dpid %s", dpid)
